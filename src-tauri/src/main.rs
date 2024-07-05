df51653a4002079161fda251949586323a6de735
Andrey Seluykov <trezortop92@gmail.com>
2024-07-05 14:38:36 +0500

    main.rs update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,7 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
 }
