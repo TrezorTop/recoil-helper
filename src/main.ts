cd74c26b81f8f331144f43a74bdf8b3e7c005bfa
Andrey Seluykov <trezortop92@gmail.com>
2024-07-05 14:38:44 +0500

    main.ts update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { invoke } from "@tauri-apps/api";
 
 createApp(App).mount("#app");
-
-const response = await invoke("greet", { name: "World" });
-
-console.log(response)
