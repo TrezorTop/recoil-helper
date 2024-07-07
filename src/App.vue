440fb8acbf519edb75605c629b6c40dcb308eb8f
Andrey Seluykov <trezortop92@gmail.com>
2024-07-07 16:21:38 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api";
+
+const hotkeys = ref<string[]>([]);
+
+const onAdd = () => {
+  hotkeys.value.push("");
+};
+
+const onKeydown = (event: KeyboardEvent, index: number) => {
+  console.log(event);
+  hotkeys.value[index] = event.key;
+};
+const onMousedown = (event: MouseEvent, index: number) => {
+  console.log(event);
+  hotkeys.value[index] = `MB_${event.button}`;
+};
+
+const onSave = () => {
+  invoke("set_hotkeys_for_action", {
+    action: "test_action",
+    hotkeys: hotkeys.value,
+  });
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div>choose hotkeys:</div>
+  <div class="hotkeys">
+    <input
+      class="hotkey"
+      v-for="(hotkey, index) in hotkeys"
+      :value="hotkeys[index]"
+      :key="hotkey"
+      @keydown="onKeydown($event, index)"
+      @mousedown="onMousedown($event, index)"
+    />
+    <button @click="onAdd">+</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  {{ hotkeys }}
+  <button @click="onSave">save</button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.hotkeys {
+  display: flex;
+  gap: 4px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.hotkey {
+  height: 21px;
+  border: 1px solid black;
 }
 </style>
+`
